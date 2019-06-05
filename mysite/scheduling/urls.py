52123993d5434864de2da7732b214f5634c5303d
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-06-05 08:41:35 -0500

    Using url patterns instead of hard-coded urls

--- mysite/scheduling/urls.py
+++ mysite/scheduling/urls.py
@@ -6,13 +6,13 @@ urlpatterns = [
     path('', scheduling.index, name='index'),
 
     path('students/', include(([
-        path('', students.room_list, name='students_room_list'),
+        path('', students.room_list, name='room_list'),
         path('<int:room_id>/', scheduling.room_detail, name='room_detail'),
         path('<int:room_id>/<int:term_id>/', scheduling.term_schedule, name='time_list'),
     ], 'scheduling'), namespace='students')),
 
     path('teachers/', include(([
-        path('', teachers.room_list, name='teachers_room_list'),
+        path('', teachers.room_list, name='room_list'),
         path('<int:room_id>/', scheduling.room_detail, name='room_detail'),
         path('<int:room_id>/<int:term_id>/', scheduling.term_schedule, name='time_list'),
     ], 'scheduling'), namespace='teachers')),
