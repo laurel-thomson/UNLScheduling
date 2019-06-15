cb83c78e449443f26f6889a405bb6a5b042342a2
Laurel Thomson <laurel.beth.thomson@gmail.com>
2019-06-15 14:06:31 +0000

    Adding python anywhere as allowed host

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '933ymr_%8l7s)i**(=rxz^o$xw1*gqzb#+a0yzv9e7_3hc-l1s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 'laurelthomson.pythonanywhere.com',]
 
 
 # Application definition
